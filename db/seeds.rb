eccc661f41a041800e447cc29a99857f10954ce1
simon hu <simon.hu.1999@gmail.com>
2023-05-24 14:09:17 +0200

    Created model, controller, migration files and schema. Added the validations. Seeded the database. Added index, show, new and create actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning the database...'
+Restaurant.destroy_all
+
+categories = %w[chinese italian japanese french belgian]
+
+puts 'Seeding you database...'
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: categories[rand(categories.size)])
+end
+puts 'Done!'
